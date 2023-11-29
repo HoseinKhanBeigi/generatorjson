052a6544e6c3dc2fc563e743bb1b577858f8c616
h.khanbeigi <h.khanbeigi@kian.digital>
2023-11-29 11:35:04 +0330

    fixe bug

--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -1,17 +1,13 @@
-import { useEffect, useState, useRef, useReducer } from "react";
+import { useRef } from "react";
 
-import html2pdf from "html2pdf.js";
 import { jsPDF } from "jspdf";
-import forntjs from "../assets/reference/Amiri-Regular.ttf";
+import forntjs from "../assets/IRANSans4/WebFonts/fonts/ttf/IRANSansWeb.ttf";
 
 import data1 from "../formDocOne.json";
 import "./schema.css";
 import Table from "./table";
 
-let hr = 1;
-
 function Sechma() {
-
   const handleMoreThanThreeDot = (item) => {
     if (item.values) {
       const resultArray = [];
@@ -37,166 +33,164 @@ function Sechma() {
   };
 
   const contentRef = useRef(null);
-  const [pageNumber, setPageNumber] = useState(1);
-  const entityRef = useRef();
-
-  const convertToPdf = async () => {
-    var opt = {
-      margin: 0,
-      filename: "myfile.pdf",
-      image: { type: "jpeg", quality: 0.5 },
-      html2canvas: { scale: 1 },
-      jsPDF: {
-        unit: "in",
-        format: "A4",
-        orientation: "portrait",
-        compress: "slow",
-      },
-    };
-
-    // html2pdf()
-    //   .set(opt)
-    //   .from(contentRef.current)
-    //   .save()
-    //   .outputPdf()
-    //   .then((pdf) => {
-    //     console.log(pdf);
-    //   });
-
-    let worker = await html2pdf()
-      .set(opt)
-      .from(contentRef.current)
-      .toPdf()
-      .output("blob")
-      .then((data) => {
-        console.log(data);
-        return data;
-      })
-      .save();
-  };
-
-  const [commonData, setCommonData] = useState(data1);
-  const handleGeneratePDF = () => {
-    // Create a new jsPDF instance with options
-    const doc = new jsPDF({
-      unit: "mm",
-    });
-
-    // Add Persian text to the PDF
-    doc.text("سلام دنیا!", 10, 10); // Example Persian text
-
-    // Save the PDF
-    doc.save("generated-pdf-with-persian-text.pdf");
-  };
 
   const makePdf = () => {
     var pdf = new jsPDF({ orientation: "portrait", unit: "mm", format: "a4" });
+    // pdf.setR2L(true); //RTL
     pdf.addFont(forntjs, "Amiri", "normal");
+
     pdf.setFont("Amiri"); // set font
+
     pdf.setFontSize(10);
+
     let y = 10;
-    let pageHeight = 0;
-    let hline = 10;
+    let pragraphHeight = y;
+    let pageHeight = 10;
     const margin = 10;
-
     const threshold = pdf.internal.pageSize.height - 2 * margin;
-
     const pageWidth = pdf.internal.pageSize.width;
     let cursorX = margin;
+
+    function reverseParentheses(sentence) {
+      return sentence.replace(/[()]/g, function (match) {
+        // Reverse the parentheses
+        return match === "(" ? ")" : "(";
+      });
+    }
+
+    function reverseDoubleAngleQuotes(sentence) {
+      return sentence.replace(/[«»]/g, function (match) {
+        // Reverse the double angle quotes
+        return match === "«" ? "»" : "«";
+      });
+    }
+
+    function reverseEnglishWords(sentence) {
+      return sentence.replace(/^[A-Za-z0-9]+\s/, "");
+    }
+
+    function reverseWord(word) {
+      // Split the word into an array of characters
+      var characters = word.split("");
+
+      // Reverse the array
+      var reversedCharacters = characters.reverse();
+
+      // Join the array back into a string
+      var reversedWord = reversedCharacters.join("");
+
+      return reversedWord;
+    }
+
+    function rearrangeSentence(sentence, url) {
+      var words = sentence.split(/\s+/);
+      var englishIndex = words.findIndex((word) => /^[A-Za-z]+$/.test(word));
+      if (englishIndex !== -1) {
+        var persianIndex = words.findIndex((word) =>
+          /^[\u0600-\u06FF]+$/.test(word)
+        );
+        if (persianIndex !== -1) {
+          words.splice(persianIndex + 1, 0, url);
+        }
+      }
+      var rearrangedSentence = words.join(" ");
+      return rearrangedSentence;
+    }
 
     data1.map((e, idx) =>
       e.pages?.map((page) => {
         page.entities.map((item, idx) => {
-          pageHeight += 47;
+          if (item?.table?.length > 5) {
+            pageHeight = threshold;
+          } else {
+            pageHeight += 25;
+          }
+
           if (pageHeight > threshold) {
-            pdf.addPage();
             y = 10;
+            pragraphHeight = y;
             pageHeight = 0;
-            hline = 10;
+            pdf.addPage();
           }
 
           const lines = pdf.splitTextToSize(item.text, 250);
+          console.log(lines, "lines");
 
-          console.log(lines);
+          let result = [];
+          let currentLine = "";
+          const arr = lines.map((item) => item.split(" ")).flat(Infinity);
+
+          for (let i = 0; i < arr.length; i++) {
+            const word = arr[i];
+            if (
+              pdf.getStringUnitWidth(currentLine) +
+                pdf.getStringUnitWidth(word) <=
+              55
+            ) {
+              if (currentLine !== "") {
+                currentLine += " ";
+              }
+              currentLine += word;
+            } else {
+              result.push(currentLine);
+              currentLine = word;
+            }
+          }
+          if (currentLine !== "") {
+            result.push(currentLine);
+          }
 
           item.id !== "table"
             ? lines.length > 1
-              ? lines.forEach((line, i) => {
-                  hline = 10;
-                  y += hline;
+              ? result.forEach((line) => {
                   const textWidth =
                     pdf.getStringUnitWidth(line) * pdf.internal.getFontSize();
                   if (cursorX + textWidth > pageWidth - margin) {
                     cursorX = margin;
                   }
-
-                  pdf.text(line, cursorX, y);
-                  cursorX += textWidth + margin; // Move cursor to the right
+                  y += 10;
+                  pdf.text(
+                    reverseParentheses(reverseDoubleAngleQuotes(line)),
+                    205,
+                    y,
+                    {
+                      align: "right",
+                    }
+                  );
                 })
-              : pdf.text(item.text, 205, y, { align: "right" })
+              : pdf.text(
+                  reverseParentheses(reverseDoubleAngleQuotes(item.text)),
+                  205,
+                  y,
+                  {
+                    align: "right",
+                    rotationDirection: 0,
+                  }
+                )
             : pdftable(item?.table, pdf, y);
 
           y += item.id !== "table" ? 10 : item?.table.length * 10 + 10;
         });
       })
     );
-
     pdf.save("generated-pdf.pdf");
   };
 
-  const handleGeneratePDF1 = () => {
-    const pdf = new jsPDF({
-      unit: "mm",
-    });
-
-    pdf.addFont(forntjs, "Amiri", "normal");
-    pdf.setFont("Amiri"); // set font
-    pdf.setFontSize(10);
-
-    const content = [
-      "موضوع بند 46 ماده 1 و ماده 11 دستورالعمل معاملات قرارداد آتی در بورس اوراق بهادار تهران، مصوب مورخ 16/12/1396 هیئت­مدیره سازمان بورس و اوراق بهادار",
-      "ششسی شسی شسی شسی شسی نمیبانست س یبنمتسی بنسنابسنتی باست سیبا سناتیب نستیب نتسنیتباسبسباتس ب",
-      ".......شیتنیباتسیتباتسابتاسی.......شسینشسیاستبیلستیبتسبغسنب سعهبهصب س بس",
-    ];
-
-    const margin = 10;
-    const pageWidth = pdf.internal.pageSize.width;
-    console.log(pageWidth);
-    let cursorX = margin;
-    let cursorY = margin;
-    let textnew = "";
-
-    content
-      .map((item) => item.split(" "))
-      .flat(Infinity)
-      .forEach((text, i) => {
-        let textWidth = pdf.getStringUnitWidth(text);
-
-        cursorY += 10;
-
-        // Draw the text
-        pdf.text(textnew, 205, cursorY, { align: "right" }); // Adjust text position
-        cursorX += textWidth + margin; // Move cursor to the right
-      });
-
-    pdf.save("generated-pdf-with-text-next-to-each-other.pdf");
-  };
-
   const pdftable = (tableContent, pdf, y) => {
-    const columnWidths = [90, 90];
+    const columnWidths = [95, 95];
 
     tableContent.forEach((row) => {
       let x = 10;
       if (row.length < 2) {
         row.forEach((cell, columnIndex) => {
           pdf.text(cell.text, x + 2, y + 8);
-          pdf.rect(x, y, 180, 10);
+          pdf.rect(x, y, 190, 10);
           x += columnWidths[columnIndex];
         });
       } else {
         row.forEach((cell, columnIndex) => {
           pdf.text(cell.text, x + 2, y + 8);
-          pdf.rect(x, y, 90, 10);
+          pdf.rect(x, y, 95, 10);
           x += columnWidths[columnIndex];
         });
       }
@@ -207,11 +201,11 @@ function Sechma() {
   return (
     <>
       <div className="btnContainer">
-        <button onClick={handleGeneratePDF1}>makePdf</button>
+        <button onClick={makePdf}>makePdf</button>
       </div>
       <div className="container" ref={contentRef}>
         <div className="pageA4">
-          {data1.map((e, idx) =>
+          {data1.map((e) =>
             e.pages?.map((page) => {
               return (
                 <div
@@ -255,7 +249,7 @@ function Sechma() {
           )}
         </div>
       </div>
-      <button onClick={convertToPdf}>Convert to PDF</button>
+      <button>Convert to PDF</button>
     </>
   );
 }
